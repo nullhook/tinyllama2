3154864ae67bc93ff9c9c0e2526db50fbd769dd8
Taher <8665427+nullhook@users.noreply.github.com>
2023-11-30 10:14:30 -0800

    lib -> frameworks

--- gen-metal.cpp
+++ gen-metal.cpp
@@ -6,7 +6,7 @@
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
-#include "lib/Metal.hpp"
+#include "frameworks/Metal.hpp"
 
 #define max(x,y) ((x>y)?x:y)
 #define int64 long
